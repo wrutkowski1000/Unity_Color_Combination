7779fc017dcc0748cb9719f3af59d41ddc42e393
wrutkowski1000 <neverwinter1000@o2.pl>
2023-01-05 05:35:42 +0100

    movingColors

--- MoveColor.cs
+++ MoveColor.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class MoveColor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject selectedObject;
+    private Transform colorStartingPosition;
+    Vector3 offset;
     void Update()
     {
-        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+            if (targetObject)
+            {
+                selectedObject = targetObject.transform.gameObject;
+                offset = selectedObject.transform.position - mousePosition;
+            }
+        }
+        if (selectedObject)
+        {
+            selectedObject.transform.position = mousePosition + offset;
+        }
+        if (Input.GetMouseButtonUp(0) && selectedObject)
+        {
+            selectedObject = null;
+        }
     }
 }
